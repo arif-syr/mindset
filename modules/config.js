a11f91bc699951d8935a35ec9e5f07d53f561520
P.S <1998punnam@gmail.com>
2023-10-04 18:47:17 -0700

    Initial setup

---
+++ modules/config.js
@@ -0,0 +1,7 @@
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0'; 
+
+module.exports = {
+  PORT,
+  HOST,
+};
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/pages/dashboard.html'));
+});
+
+router.get('/focus', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/pages/focus.html'));
+});
+
+module.exports = router;
