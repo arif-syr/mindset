7c8e2220ac26872dbf61696048fb2460876d7f48
P.S <1998punnam@gmail.com>
2023-10-05 17:06:14 -0700

    focus functionality

--- public/scripts/focus.js
+++ public/scripts/focus.js
@@ -48,9 +48,62 @@ $(document).ready(function () {
                          <td>${task.name}</td>
                          <td>${task.description}</td>
                          <td>${task.duration} hours</td>
-                         <td><a href="/pages/startTask.html?duration=${task.duration}" class="btn btn-success">Start Task</a></td>
+                         <td><span id="timer-${index}">00:00</span></td>
+                     <td>
+                       <button class="btn btn-success start-pause" data-index="${index}" data-status="start">Start</button>
+                       <button class="btn btn-warning restart" data-index="${index}">Restart</button>
+                     </td>
                        </tr>`;
             tableBody.append(row);
           });
+          attachEventHandlers();
     }
+
+    function attachEventHandlers() {
+      // Start/Pause button handler
+      $(".start-pause").click(function() {
+          let btn = $(this);
+          let index = btn.data("index");
+          let status = btn.data("status");
+          let timerElement = $(`#timer-${index}`);
+          let duration = tasks[index].duration * 60;  
+  
+          if (status === "start") {
+              btn.data("status", "pause");
+              btn.text("Pause");
+              tasks[index].interval = setInterval(function() {
+                  if (duration > 0) {
+                      duration--;
+                      let minutes = Math.floor(duration / 60);
+                      let seconds = duration % 60;
+                      timerElement.text(`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`);
+                  } else {
+                      clearInterval(tasks[index].interval);
+                      btn.text("Done");
+                      btn.prop("disabled", true);
+                  }
+              }, 1000);
+          } else {
+              btn.data("status", "start");
+              btn.text("Start");
+              clearInterval(tasks[index].interval);
+          }
+      });
+  
+      // Restart button handler
+      $(".restart").click(function() {
+          let index = $(this).data("index");
+          let timerElement = $(`#timer-${index}`);
+          let startPauseBtn = $(`.start-pause[data-index="${index}"]`);
+          
+          if (tasks[index].interval) {
+              clearInterval(tasks[index].interval);
+          }
+  
+          timerElement.text("00:00");
+          startPauseBtn.text("Start");
+          startPauseBtn.data("status", "start");
+          startPauseBtn.prop("disabled", false);
+      });
+  }
 });
