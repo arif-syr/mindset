6830c50a8917ffec19f8ce4a7bedee3e224a29bd
arif-ariff <126988341+arif-ariff@users.noreply.github.com>
2023-10-06 17:40:34 -0700

    Enhance addiction functionality

--- public/scripts/addiction.js
+++ public/scripts/addiction.js
@@ -1,88 +1,43 @@
 $(document).ready(function () {
-  $('#contact_form').bootstrapValidator({
-    feedbackIcons: {
-      valid: 'glyphicon glyphicon-ok',
-      invalid: 'glyphicon glyphicon-remove',
-      validating: 'glyphicon glyphicon-refresh'
-    },
-    fields: {
-      addiction_name: {
-        validators: {
-          stringLength: {
-            min: 1,
-          },
-          notEmpty: {
-            message: 'Please supply addiction name'
-          }
-        }
-      },
-      quit_date: {
-        validators: {
-          date: {
-            format: 'MM-DD-YYYY',
-            message: 'The date is not valid'
-          },
-          notEmpty: {
-            message: 'Please supply your quit date'
-          },
-          callback: {
-            message: 'Quit date must be later than today',
-            callback: function (value, validator, $field) {
-              var today = new Date().toLocaleDateString('en-US');
-              return new Date(value) > new Date(today);
-            }
-          }
-        }
-      },
-      savings_money: {
-        validators: {
-          notEmpty: {
-            message: 'Please supply a dollar amount'
-          },
-          numeric: {
-            message: 'The value must be a number'
-          },
-          greaterThan: {
-            value: 0,
-            inclusive: true,
-            message: 'Enter a non-negative value'
-          }
-        }
-      },
-      phone: {
-        validators: {
-          notEmpty: {
-            message: 'Please supply your phone number'
-          }
-        }
-      },
-      comment: {
-        validators: {
-          stringLength: {
-            min: 10,
-            max: 2000,
-            message: 'Please enter at least 10 characters and no more than 2000'
-          },
-          notEmpty: {
-            message: 'Please supply a description of your reasons'
-          }
-        }
-      }
-    }
-  })
-    .on('success.form.bv', function (e) {
-      e.preventDefault();
-      var $form = $(e.target);
-      var bv = $form.data('bootstrapValidator');
-      $.post($form.attr('action'), $form.serialize(), function (result) {
-        console.log(result);
-        if (result.success) {
-          $('#success_message').slideDown({ opacity: "show" }, "slow")
-          $('#contact_form').data('bootstrapValidator').resetForm();
-        } else {
-          $('#errorMessage').text('Form submission failed. Please try again.');
-        }
-      }, 'json');
-    });
-});
+  $("#navbar").load("/pages/navbar.html");
+  $("#footer").load("/pages/footer.html");
 
+  $('#contact_form').on('submit', function (e) {
+    e.preventDefault();
+
+    const addiction_name = $("#addiction_name").val();
+    if (!addiction_name) {
+      alert("Please provide an addiction name.");
+      return;
+    }
+
+    const quit_date = $("#quit_date").val();
+    if (!quit_date) {
+      alert("Please provide a quit date.");
+      return;
+    }
+
+    const savings_money = $("#savings_money").val();
+    if (!savings_money || isNaN(savings_money) || parseFloat(savings_money) < 0) {
+      alert("Please provide a valid savings amount.");
+      return;
+    }
+
+    const reasons = $("#reasons").val();
+    if (!reasons) {
+      alert("Please provide reasons for quitting.");
+      return;
+    }
+
+    $.post('/saveAddiction', $(this).serialize(), function (result) {
+      if (result.success) {
+        $('#success_message').removeClass('d-none');
+        $('#contact_form')[0].reset();
+      } else {
+        alert('Failed to save data. ' + (result.message || ''));
+      }
+    }).fail(function () {
+      alert('Failed to save data. Please try again.');
+    });
+  });
+});
--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,4 +1,43 @@
 $(document).ready(function () {
     $("#navbar").load("/pages/navbar.html");
     $("#footer").load("/pages/footer.html");
+    $.get('/getAddiction', function (result) {
+        if (result.success) {
+            const data = result.data;
+            const html = `
+                <h3>Addiction: ${data.addiction_name}</h3>
+                <p>Quit Date: ${data.quit_date}</p>
+                <p>Savings: ${data.savings_money}</p>
+                <p>Phone: ${data.phone}</p>
+                <p>Reasons: ${data.reasons}</p>
+            `;
+            $('#addictionData').html(html);
+
+            let quitDate = new Date(data.quit_date);
+            let now = new Date();
+
+            if (quitDate < now) {
+                setInterval(function () {
+                    now = new Date();
+                    let diff = Math.abs(now - quitDate);
+
+                    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+                    diff -= days * (1000 * 60 * 60 * 24);
+
+                    let hours = Math.floor(diff / (1000 * 60 * 60));
+                    diff -= hours * (1000 * 60 * 60);
+
+                    let mins = Math.floor(diff / (1000 * 60));
+                    diff -= mins * (1000 * 60);
+
+                    let secs = Math.floor(diff / (1000));
+
+                    document.getElementById('timerDisplay').innerText = `${days} Days, ${hours}:${mins}:${secs} elapsed since quit date`;
+                }, 1000);
+            } else {
+                document.getElementById('timerDisplay').innerText = "N/A";
+            }
+        }
+    });
+
 });
